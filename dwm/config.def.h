6af6d1477d829f45dee713ddee648fb0a3645ad7
XwX12596 <1259685860@qq.com>
2023-07-27 00:21:46 +0800

    scrot --> flameshot

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -68,11 +68,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *scrotcmd[]  = { "scrot", "-s", "/home/xwx/Media/snapshot/%Y%m%d%H%M%S.png", NULL };
+// static const char *scrotcmd[]  = { "scrot", "-s", "/home/xwx/Media/snapshot/%Y%m%d%H%M%S.png", NULL };
+static const char *flameshotcmd[]  = { "flameshot", "gui", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = scrotcmd } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = flameshotcmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
